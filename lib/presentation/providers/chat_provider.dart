2c3edad2ac60498aa548e7eac0d09a94b580fe86
cituk <ecituk@thefives.com.mx>
2023-07-06 22:57:10 -0500

    Mostrar los mensajes del Provider

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> messages = [
+  List<Message> messageList = [
     Message(text: 'Hola cielo', fromWho: FromWho.me ),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me ),
   ];
